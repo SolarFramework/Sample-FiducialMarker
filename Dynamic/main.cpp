dc0ec469157b7b1433413fc67512a4524caf1850
BCOM\jroyan <jerome.royan@b-com.com>
2018-04-16 15:27:33 +0200

    Change pose estimation component from UUID::POSE_ESTIMATION to UUID:POSE_ESTIMATION_PNP

--- Dynamic/main.cpp
+++ Dynamic/main.cpp
@@ -68,7 +68,7 @@ void marker_run(int argc,char** argv){
     SRef<features::IDescriptorMatcher> patternMatcher = opencvModule.createComponent<features::IDescriptorMatcher>(MODULES::OPENCV::UUID::DESCRIPTOR_MATCHER_RADIUS);
     SRef<features::ISBPatternReIndexer> patternReIndexer = toolsModule.createComponent<features::ISBPatternReIndexer>(MODULES::TOOLS::UUID::SBPATTERN_REINDEXER);
     SRef<geom::IImage2WorldMapper> img2worldMapper = toolsModule.createComponent<geom::IImage2WorldMapper>(MODULES::TOOLS::UUID::IMAGE2WORLD_MAPPER);
-    SRef<solver::pose::I3DTransformFinder> PnP = opencvModule.createComponent<solver::pose::I3DTransformFinder>(MODULES::OPENCV::UUID::POSE_ESTIMATION);
+    SRef<solver::pose::I3DTransformFinder> PnP = opencvModule.createComponent<solver::pose::I3DTransformFinder>(MODULES::OPENCV::UUID::POSE_ESTIMATION_PNP);
     SRef<display::I3DOverlay> overlay3D = opencvModule.createComponent<display::I3DOverlay>(MODULES::OPENCV::UUID::OVERLAY3D);
     SRef<display::I2DOverlay> overlay2D = opencvModule.createComponent<display::I2DOverlay>(MODULES::OPENCV::UUID::OVERLAY2D);
 
