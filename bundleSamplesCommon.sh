69c469d2ce633aba999793032f9efe5ad6ffa258
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2021-06-10 18:00:41 +0200

    Do not modify conf files in place for Linux when bundling
    Work on a copy instead.
    Otherwise git status is dirty.

--- bundleSamplesCommon.sh
+++ bundleSamplesCommon.sh
@@ -73,24 +73,27 @@ if [ "$PLATFORM" == "" ]; then
    exit 1
 fi
 
-
-if [ "$PLATFORM" == "linux" ]; then
-   # Update configuration files by replacing win-cl-1.1 by linux in module paths
-   echo "**** Update module path in configuration file (win-cl-14.1 -> linux-gcc)"
-   for f in `find . -name "*_conf*.xml"` ; do sed -i 's/win-cl-14.1/linux-gcc/' $f; done
-fi
-
 echo "**** Install dependencies locally"
 remaken install packagedependencies.txt
 remaken install packagedependencies.txt -c debug
 
 echo "**** Bundle dependencies in bin folder"
-for file in './SolARPipeline_FiducialMarker_Runner/SolARPipeline_FiducialMarker_Runner_conf.xml',\
-            './SolARStandAlone_FiducialMarker_Mono/SolARStandAlone_FiducialMarker_Mono_conf.xml'
+for f in `find . -not \( -path "./bin" -prune \) -not \( -path "./bin-test" -prune  \) -name "*_conf*.xml"`
 do
+   file=$f
+   # Work on copy to avoid polluting the git status
+   if [ "$PLATFORM" == "linux" ]; then
+        sed 's/win-cl-14.1/linux-gcc/' $f > tmp_$f
+        file=tmp_$f
+   fi
+
    echo "install dependencies for config file: $file"
    remaken bundleXpcf $file -d ./bin/Release -s modules
    remaken bundleXpcf $file -d ./bin/Debug -s modules -c debug
+
+   if [ "$PLATFORM" == "linux" ]; then
+        rm tmp_$f
+   fi
 done
 
 if [ "$PLATFORM" == "linux" ]; then
