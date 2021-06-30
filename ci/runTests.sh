6161b32b46fb3b78c1415afe20957157c2f84d94
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2021-06-30 18:43:05 +0200

    Revert "Add call to installData.sh in runTests.sh"
    It's too late, installData.sh must be called before
    building the project because it is this step that will
    copy the data in the deployment dir.
    
    This reverts commit f94af9ae873f9b9f3ba2b8f7dca1e2f7fcafdf7e.

--- ci/runTests.sh
+++ ci/runTests.sh
@@ -127,12 +127,6 @@ function run()
 # MAIN
 #
 
-# TODO: specify these kind of steps in JenkinsFile ?
-if ! sh $SCRIPT_DIR/../installData.sh; then
-        echo "ERROR: could not install data"
-        exit 1
-fi
-
 if [ "$CONFIG" == "all" ]; then
     run release
     run debug
